f9ebcb1fa48dbf353b508c10f385381647e5d559
ut <ulf.treger@googlemail.com>
2021-11-30 19:23:19 +0100

    Basic setup for persistent params based map navigation

--- app/assets/javascripts/helpers/user.js
+++ app/assets/javascripts/helpers/user.js
@@ -2,8 +2,27 @@ function UserZoomLevel(map) {
   map.on('zoomend',function(e){
     var zl  = map.getZoom();
     localStorage.setItem('UserZoomLevel',zl);
+    console.log(e.target.getZoom());
+    console.log(e.target.getCenter());
+    var newUrl = addParam(window.location.href, "zoom", zl);
+    console.log(newUrl);
+    window.history.pushState(null, document.title, newUrl);
+  });
+  map.on('moveend',function(e){
+    console.log(e.target.getCenter());
+    var latlng = e.target.getCenter();
+    var newUrl = addParam(window.location.href, "lat", latlng.lat);
+    newUrl = addParam(newUrl, "lon", latlng.lng);
+    console.log(newUrl);
+    window.history.pushState(null, document.title, newUrl);
   });
 }
+function addParam(currentUrl,key,val) {
+    var url = new URL(currentUrl);
+    url.searchParams.set(key, val);
+    return url.href;
+}
+
 function UserMapBounds(map) {
   map.on('dragend',function(e){
     var bounds = map.getBounds();
